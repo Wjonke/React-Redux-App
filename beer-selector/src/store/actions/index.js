b68f8d49c74c8d1545bfdc29595acf3d3fc8b84e
Wes Jonke <wesjonke@gmail.com>
2019-07-25 16:49:15 -0400

    created actions for fetching beer!

---
+++ beer-selector/src/store/actions/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const FETCH_BEER_START =   'FETCH_BEER_START';
+export const FETCH_BEER_SUCCESS = 'FETCH_BEER_SUCCESS';
+export const FETCH_BEER_FAILURE = 'FETCH_BEER_FAILURE';
+
+
+
+
+export const fetchBeer = () => dispatch => {
+  dispatch({type: FETCH_BEER_START});
+
+  axios
+    .get('https://api.punkapi.com/v2/beers/random')
+
+    .then(res => {
+      dispatch({type:FETCH_BEER_SUCCESS, payload: res.data});
+    })
+    
+    .catch((err => console.log(err.response)))
+
+}
+
+
