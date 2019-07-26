4049d315bc1e58f2f3274adb1b921e8e46a52e09
Wes Jonke <wesjonke@gmail.com>
2019-07-25 21:20:34 -0400

    added in basic styling, needs refined at a later date

--- beer-selector/src/components/Beers.js
+++ beer-selector/src/components/Beers.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 import Loader from "react-loader-spinner";
 
 import {fetchBeer} from '../store/actions/index'
+import { Container, ImageContainer, Warning, WarningText, Button } from '../AppStyles.js';
 
 
 const Beers = props => {
@@ -25,40 +26,50 @@ const Beers = props => {
 if (props.beer !== '') {
   return (
     <div>
-
-      <div>
-        <h1>Enjoy!</h1>
-        <h2>Check out the food pairings below for dinner ideas</h2>
-      </div>
-
-      <div>
-        <p>Is this beer not for you?</p>
-        <button onClick={getBeer}>Pick Another!</button>
-      </div>
-
-      <div>
-        <h1>{props.beer.name}</h1>
-        <h2>{props.beer.tagline}</h2>
-        <img 
-          height="300px" 
-          width="auto" 
-          src={props.beer.image_url} 
-          alt="pic of beer listed"
-        />
-        <p> ABV:   {props.beer.abv}</p>
-        <p> Description: {props.beer.description}</p>
-        <p> Food Pairing: 
-          {props.beer.food_pairing.map(food => {
-            return <li key={props.beer.id}>{food}</li>
-            // key is not unique due to duplicate data in API
-          })}
-        </p>
-
+      <Container>
         <div>
-          <h3>***Disclaimer***</h3>
-          <p>We are not responsible for what happens if you ask your wife to fetch this beer.</p>
+          <h1>Enjoy!</h1>
+          <h2>Check out the food pairings below for dinner ideas</h2>
         </div>
-        
+
+        <p>Is this beer not for you?</p>
+        <Button onClick={getBeer}>Pick Another!</Button>
+      </Container>
+
+      <div>
+        <Container> 
+          <h1>{props.beer.name}</h1>
+          <h2>{props.beer.tagline}</h2>
+          <p> ABV:   {props.beer.abv}</p>
+        </Container>
+
+        <ImageContainer>
+          <img 
+            height="300px" 
+            width="auto" 
+            src={props.beer.image_url} 
+            alt="pic of beer listed"
+          />
+        </ImageContainer>
+
+        <Container>
+          <p> Description: {props.beer.description}</p>
+          <p> Food Pairing: 
+            <ul>
+              {props.beer.food_pairing.map(food => {
+              return <li >{food}</li>
+              // key is not unique due to duplicate data in API
+              })}
+            </ul>
+          </p>
+        </Container>
+
+        <Container>
+          <div>
+            <Warning>***Disclaimer***</Warning>
+            <WarningText>We are not responsible for what happens if you ask your wife to fetch this beer.</WarningText>
+          </div>
+        </Container>
       </div>
 
     </div> 
@@ -67,14 +78,14 @@ if (props.beer !== '') {
 } else{
     return(
       <div>
-        <div>
+        <Container>
           <h1>Long Day and need a drink?</h1>
           <h2>Click below and let us pick one for you!</h2>
-        </div>
-
-        <div>
-          <button onClick={getBeer}>Send It!</button>
-        </div>
+      
+          <div>
+            <Button onClick={getBeer}>Send It!</Button>
+          </div>
+        </Container>
       </div>
   )
 }
